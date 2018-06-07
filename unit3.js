ff0ef0e2f388b23a9ec9085216153b8ac9650692
Adrian Acosta <aacosta@techlaunch>
2018-06-07 08:36:59 -0400

    second commit

--- unit3.js
+++ unit3.js
@@ -6,20 +6,20 @@
 
 // reduce(nums, add, 0);   //-> 8
 // Here's how it works. The function has an "accumulator value" which starts as the initialValue and accumulates the output of each loop. The array is iterated over, passing the accumulator and the next array element as arguments to the callback. The callback's return value becomes the new accumulator value. The next loop executes with this new accumulator value. In the example above, the accumulator begins at 0. add(0,4) is called. The accumulator's value is now 4. Then add(4, 1) to make it 5. Finally add(5, 3) brings it to 8, which is returned. 
-// var nums = [4, 1, 3];
-// var int = 0;
-// var add = function (a, b) { return a + b; }
+var nums = [4, 1, 3];
+var int = 0;
+var add = function (a, b) { return a + b; }
 
-// function reduce(nums, add, int) {
+function reduce(nums, add, int) {
 
-//     for (var i = 0; i < nums.length; i++) {
-//         int = add(int, nums[i]);
-//     };
+    for (var i = 0; i < nums.length; i++) {
+        int = add(int, nums[i]);
+    };
 
-//     return int;
-// };
+    return int;
+};
 
-// console.log(reduce(nums, add, 0))   //-> 8
+console.log(reduce(nums, add, 0))   //-> 8
 
 
 // Challenge 5
